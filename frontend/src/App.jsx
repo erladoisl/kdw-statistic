ee97cf83dbd934f3c48e5530e35bbb2eed3faac2
Rakhina Latypova <rakhina.bdd@gmail.com>
2022-03-25 15:38:30 +0300

    + footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Statistic from './components/Statistic/Statistic';
 
@@ -7,6 +8,7 @@ function App() {
     <div id="wrapper">
       <Header />
       <Statistic />
+      <Footer />
     </div>
   );
 };
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,52 @@
+
+const Footer = () => {
+
+    return (
+        <div className="contacts">
+            <div className="container">
+
+                <div className="container" >{/*style="color: white">*/}
+
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h2 className="mb-3"><span>Контакты:</span> +7 (843) 5-333-776</h2>
+                            <h2 className="mb-3"><span>E-mail:</span> org@kazandigitalweek.com</h2>
+                        </div>
+                    </div>
+
+                    <div className="row">
+
+                        <div className="col-md-6 col-lg-3 text-white"><h6 className="pb-0 mb-0 pt-4">По вопросам партнерства и участия в выставке:</h6>
+                            <br />Гильмутдинова Алсу Азатовна
+                            <br />Тел: +7 (987) 215-11-00
+                            <br />e-mail: Alsu.gilmutdinova@tatar.ru
+                        </div>
+
+                        <div className="col-md-6 col-lg-3 text-white"><h6 className="pb-0 mb-0 pt-4">По организационным вопросам:</h6><br />
+                            <br />Аникина Наталья Сергеевна
+                            <br />Тел: +7 (951) 892-80-70
+                            <br />e-mail: org@kazandigitalweek.ru
+                        </div>
+
+                        <div className="col-md-6 col-lg-3 text-white"><h6 className="pb-0 mb-0 pt-4">По вопросам публикации статей:</h6><br />
+                            <br />Галиева Сирина Габдулахатовна
+                            <br />Тел: +7 (917) 923-73-05
+                            <br />e-mail: org@kazandigitalweek.ru
+                        </div>
+
+                        <div className="col-md-6 col-lg-3 text-white">
+                            <h6 className="pb-0 mb-0 pt-4">По вопросам проведения технических визитов:</h6>
+                            <br />Шакиров Булат Ленарович
+                            <br />Тел: +7 (987) 188-00-03
+                            <br />B.shakirov@tatar.ru
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default Footer
