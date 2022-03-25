bd537797c8f521cae41618b1c33a68e28d569ea9
Rakhina Latypova <rakhina.bdd@gmail.com>
2022-03-25 11:39:32 +0300

    rf

--- statistic/views.py
+++ statistic/views.py
@@ -7,20 +7,16 @@ from dbapi.users import getRegisterationCount, getTotalRegistrationCount, getLoc
 
 class RegistrationCountView(APIView):
     def get(self, request):
-        print('RegistrationCountView GET')
         start_date = request.GET.get('startDate', (datetime.now() - relativedelta(years=1)).strftime('%Y-%m-%d'))
         end_date = request.GET.get('endDate', datetime.now().strftime('%Y-%m-%d'))
         page = int(request.GET.get('page_num', 0))
         data = getRegisterationCount(start_date, end_date, page)
         
-        print(f'start_date: {start_date} end_date: {end_date}')
-        print(f'result: {data.to_json()}')
         return Response({'data': data.to_json()})
 
 
 class TotalRegistrationCountView(APIView):
     def get(self, request):
-        print('TotalRegistrationCountView GET')
         start_date = request.GET.get('startDate', (datetime.now() - relativedelta(years=1)).strftime('%Y-%m-%d'))
         end_date = request.GET.get('endDate', datetime.now().strftime('%Y-%m-%d'))
 
@@ -31,7 +27,6 @@ class TotalRegistrationCountView(APIView):
 
 class LocationCountView(APIView):
     def get(self, request):
-        print('LocationCountView GET')
         start_date = request.GET.get('startDate', (datetime.now() - relativedelta(years=1)).strftime('%Y-%m-%d'))
         end_date = request.GET.get('endDate', datetime.now().strftime('%Y-%m-%d'))
 
@@ -42,8 +37,7 @@ class LocationCountView(APIView):
 
 class MonthsView(APIView):
     def get(self, request):
-        print('MonthsView GET')
 
-        data = getLocationStatistic()
+        data = getYearStatistic()
 
         return Response({'data': data.to_json()})
