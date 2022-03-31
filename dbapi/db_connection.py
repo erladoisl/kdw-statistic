0ee3d09b69b074180d50a8c9d6beb3d3a5a879b3
Rakhina Latypova <rakhina.bdd@gmail.com>
2022-03-31 09:32:47 +0300

    rf

--- dbapi/db_connection.py
+++ dbapi/db_connection.py
@@ -2,10 +2,12 @@ import psycopg2
 import dbapi.config as c
 
 conn_string = f"host={c.PGHOST} " +\
-    "port=5432 " +\
+              f"port=5432 " +\
               f"dbname={c.PGDATABASE} " +\
               f"user={c.PGUSER} " +\
               f"password={c.PGPASSWORD}"
 conn = psycopg2.connect(conn_string)
+
+
 
 cursor = conn.cursor()
